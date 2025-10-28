f93a003977dd2b985d3bd6f02d0f103e91f0a925
Jessin Sunny <119120870+Jessin-Sunny@users.noreply.github.com>
2025-10-28 12:48:08 +0530

    Update constant.c

--- Experiment 13/constant.c
+++ Experiment 13/constant.c
@@ -10,11 +10,11 @@ typedef struct
 {
 	char op;
 	char op1[OPNDSIZE];
-	char op2[OPNDSIZE];
+	char op2[OPNDSIZE];	
 	char res[OPNDSIZE];
 } quadruple;
 quadruple quads[MAXQUADS];
-bool isint(char*s)
+bool isint(char*s) //Check for integer constant values
 {
     while(*s)
     {
@@ -24,7 +24,7 @@ bool isint(char*s)
     }
     return true;
 }
-
+//Evaluate the given three address expression
 int calculate(char *op1,char *op2, char optr)
 {
   int retval = 0;
@@ -41,14 +41,14 @@ int calculate(char *op1,char *op2, char optr)
                 break;
     case '/': 
                 if(atoi(op2)!=0)
-                    retval = atoi(op1)+atoi(op2);
+                    retval = atoi(op1)/atoi(op2);
                 break;
     default:
                 break;
   }
   return retval;
 }
-char *getconstval(char *opnd)
+char *getconstval(char *opnd) //Get the constant value after folding
 {
   for(int i=0;i<numquads;i++)
   {
@@ -57,11 +57,11 @@ char *getconstval(char *opnd)
   }
   return opnd;
 }
-void fold()
+void fold() //Evaluate all constant expressions and store back
 {
   for(int i=0;i<numquads;i++)
   {
-    if(isint(quads[i].op1)&&isint(quads[i].op2))
+    if(isint(quads[i].op1)&&isint(quads[i].op2))  //fold
     {
      int result = calculate(quads[i].op1,quads[i].op2,quads[i].op);
      quads[i].op='=';
@@ -70,25 +70,25 @@ void fold()
     }
   }
 }
-void propogate()
+void propogate() //Propogate the evaluated constant values
 {
   for(int i=0;i<numquads;i++)
   {
     strcpy(quads[i].op1,getconstval(quads[i].op1));
     strcpy(quads[i].op2,getconstval(quads[i].op2));
-    if(isint(quads[i].op1)&&isint(quads[i].op2))
-      fold();
+    if(isint(quads[i].op1)&&isint(quads[i].op2)) // fold again if both opnds became integers
+      fold(); 
   }
 }
 int main()
 {
-    memset(quads,0,sizeof(quads));
+    memset(quads,0,sizeof(quads)); //clear the quadruples table
     printf("Enter the number of 3-address instructions\n");
     scanf("%d",&numquads);
     printf("Enter each instruction\n e.g.\n + 2 3 a\n = 3 - b\n");
     for(int i=0;i<numquads;i++)
     {
-        getchar();
+        getchar(); //Remove stray new lines
         quads[i].op=getchar();
         scanf("%s %s %s",quads[i].op1,quads[i].op2,quads[i].res);
     }
